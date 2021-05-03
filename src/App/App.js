01839b66a402ccd4458df6547e83bdbdbcb79480
Nishat Hoque <hoquen0397@gmail.com>
2021-05-03 08:58:07 -0400

    tests

--- src/App/App.js
+++ src/App/App.js
@@ -38,7 +38,6 @@ class App extends Component {
                             description = {image.description}
                         />,
                     )}
-                
                 </div>
             </div>
         )
--- src/Thumbnail/Thumbnail.test.js
+++ src/Thumbnail/Thumbnail.test.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import Thumbnail from './Thumbnail'
 
 it('renders wihtout crashing', () => {
     const div = document.createElement('div')
-    ReactDOM.render(<App/>, div)
+    ReactDOM.render(<Thumbnail/>, div)
     ReactDOM.unmountComponentAtNode(div)
 })
